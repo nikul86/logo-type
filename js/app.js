6a65b04c6657b4b3c11ed72c6793b0f665133060
nikulin86 <68349757+nikulin86@users.noreply.github.com>
2021-01-18 14:32:43 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,5 +13,14 @@ $(function(){
         $('.header__menu ul').slideToggle();
     })
 
-    
+    // карта появляется после тго как доскролили до этого элементы
+    let news = $('.news');
+    let newsTop = news.offset().top;
+    $(window).bind('scroll', function(){
+        let windowTop = $(this).scrollTop();
+        if(windowTop > newsTop) {
+            $('#footer__map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac78ccb2d9dcebbdb647985bd00f6bb1b7b7206d8dcef7456a02bfa2e581820d4&amp;width=100%&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>')
+            $(window).unbind('scroll')
+        }
+    })
 });
